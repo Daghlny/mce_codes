525290df96f457d59d3b5784312312153cfc887e
Daghlny <daghlny@gmail.com>
2017-05-15 17:22:06 +0800

    full time records version

--- BMBK.cpp
+++ BMBK.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <cstdint>
 #include <vector>
+#include <set>
 #include <utility>
 #include <algorithm>
 #include <iostream>
@@ -22,6 +23,7 @@ using std::vector;
 using std::cout;
 using std::endl;
 using std::pair;
+using std::set;
 
 BMBK::BMBK():
     top(0)
@@ -46,12 +48,17 @@ BMBK::BMBK(const char *gfilename, const char *dfilename, vid nodenum):
 int
 BMBK::compute()
 {
-    struct timeval tbeg, tend;
+    struct timeval tbeg, tend, pbeg, pend;
+    struct timeval aobeg, aoend, cpbeg, cpend;
     gettimeofday(&tbeg, NULL);
     localbitVector R(g.nodenum);
+    localbitVector lbvector(g.nodenum);
     gettimeofday(&tend, NULL);
     pair<int,int> p = get_running_usec(tbeg, tend);
     pair<int,int> f = get_running_usec(tbeg, tend);
+    pair<int,int> pt = {0,0};
+    pair<int,int> ao = {0,0};
+    pair<int,int> cp = {0,0};
     
     int count = 0;
     for ( int i = 0; i < g.nodenum; ++i )
@@ -72,6 +79,9 @@ BMBK::compute()
         gettimeofday(&t1, NULL);
         */
 
+        //FIX: how to manage this memory
+        int *Rstack = new int[degree+2];
+        memset(Rstack, 0, sizeof(int) * (degree+2));
         //FIX: a direct variable access should be optimized
         bitMatrix Pmat(degree+2, nbhood.get_nodenum());
         //FIX: this phase can be optimized
@@ -89,15 +99,59 @@ BMBK::compute()
         while ( top >= 0 )
         {
             int v = -1;
-            //cout << "top: " << top << endl;
+            /*
+            cout << "top: " << top << endl;
+            cout << "Pmat: " << Pmat[top].to_string() << endl;
+            cout << "Xmat: " << Xmat[top].to_string() << endl;
+            */
+            //FIX: here can be optimized
             if ( ( v = Pmat[top].first(1)) != -1 )
             {
-                Pmat[top].setbit(v, 0);
+                /* Pivot Selection */
+                gettimeofday(&pbeg, NULL);
+                list<int> inds;
+
+                gettimeofday(&aobeg, NULL);
+                Pmat[top].allone(inds);
+                gettimeofday(&aoend, NULL);
+                pair<int,int> aotime = get_running_usec(aobeg, aoend);
+                add_running_usec(ao, aotime);
+
+                //Xmat[top].allone(inds);
+                set<int> indset(inds.begin(), inds.end());
+                int max_num_neighbors = -1;
+                int pivot = 0;
+
+                gettimeofday(&cpbeg, NULL);
+                for ( auto indit = indset.begin(); indit != indset.end(); ++indit )
+                {
+                    lbvector.setWithBitAnd(nbhood[*indit], Pmat[top]);
+                    int onenum = lbvector.allone();
+                    if ( onenum > max_num_neighbors )
+                    {
+                        pivot = *indit;
+                        max_num_neighbors = onenum;
+                    }
+                    //printf("node: %d degree: %d inds.size: %d max_nbors: %d\n", i, degree, indset.size(), max_num_neighbors);
+                }
+                gettimeofday(&cpend, NULL);
+                pair<int,int> cptime = get_running_usec(cpbeg, cpend);
+                add_running_usec(cp, cptime);
+
+                gettimeofday(&pend, NULL);
+                pair<int,int> pttime = get_running_usec(pbeg, pend);
+                add_running_usec(pt, pttime);
+
+                //printf("pivot: %d\n", pivot);
+                /* End of Pivot Selection */
+
+                Pmat[top].setbit(pivot, 0);
                 //FIX: this sentence maybe has wrong
-                Pmat[top+1].setWithBitAnd(nbhood[v], Pmat[top]);
-                Xmat[top].setbit(v, 1);
-                Xmat[top+1].setWithBitAnd(nbhood[v], Xmat[top]);
-                R.setbit(v, 1);
+                Pmat[top+1].setWithBitAnd(nbhood[pivot], Pmat[top]);
+                Xmat[top].setbit(pivot, 1);
+                Xmat[top+1].setWithBitAnd(nbhood[pivot], Xmat[top]);
+                Rstack[top] = pivot;
+                R.setbit(pivot, 1);
                 top++;
             } 
             else {
@@ -106,30 +160,23 @@ BMBK::compute()
                     //you should output @R here as a maximal clique
                     ++count;
                 }
+                R.setbit(Rstack[top], 0);
                 top--;
-                R.setlastone();
-                /*
-                int last_one_pos = R.last(1);
-                if ( last_one_pos != -1 )
-                    R.setbit(last_one_pos, 0);
-                    */
+                //R.setlastone();
             }
         }
         gettimeofday(&tend, NULL);
         pair<int,int> ctime = get_running_usec(tbeg, tend);
         add_running_usec(f, ctime);
-        /*
-        struct timeval t2;
-        gettimeofday(&t2, NULL);
-        int totalusec = (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec);
-        cout << " Time: " << totalusec / 1000000 << "s " << totalusec % 1000000 << "us" << endl;
-        */
+        delete[] Rstack;
     }
 
     printf("##### Compute Report #####\n");
-    printf("preprocess time: %d s %d us\n", p.first, p.second);
-    printf("compute time:    %d s %d us\n", f.first, f.second);
-    //cout << "There are total " << count << " vertices have been computed" << endl;
+    printf("preprocess time:    %d s %d us\n", p.first, p.second);
+    printf("compute time:       %d s %d us\n", f.first, f.second);
+    printf("pivot selection:    %d s %d us\n", pt.first, pt.second);
+    printf("allone time:        %d s %d us\n", ao.first, ao.second);
+    printf("compare pivot time: %d s %d us\n", cp.first, cp.second);
     return count;
 }
 
